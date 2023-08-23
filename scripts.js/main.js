12aeae3b0952bfbab61ad9f5fb92b23169478058
Matvei <joflish@yandex.ru>
2023-08-23 13:48:10 +0500

    добавил линтер

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -1,20 +1,25 @@
 const difficultyLevels = [
-document.getElementById('radio1'),
-document.getElementById('radio2'),
-document.getElementById('radio3'),
+  document.getElementById("radio1"),
+  document.getElementById("radio2"),
+  document.getElementById("radio3"),
 ];
-const buttonStart = document.getElementById('start-button');
+const buttonStart = document.getElementById("start-button");
 
+let main = document.getElementById("main");
 let selectedDifficultyLevel;
 
 difficultyLevels.forEach((level, index) => {
-level.addEventListener("click", () => {
+  level.addEventListener("click", () => {
     selectedDifficultyLevel = index + 1;
-});
+  });
 });
 
 buttonStart.addEventListener("click", () => {
-    console.log(selectedDifficultyLevel);
+  if (selectedDifficultyLevel === 1) {
+    main.innerHTML = `Вы выбрали легкий уровень`;
+  } else if (selectedDifficultyLevel === 2) {
+    main.innerHTML = `Вы выбрали средний уровень`;
+  } else if (selectedDifficultyLevel === 3) {
+    main.innerHTML = `Вы выбрали сложный уровень`;
+  }
 });
-
-
