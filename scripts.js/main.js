6b702532b02616c648e27e760f0b5e57ec20842e
Matvei <joflish@yandex.ru>
2023-09-01 16:53:44 +0500

    message

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -1,25 +1,25 @@
 const difficultyLevels = [
-  document.getElementById("radio1"),
-  document.getElementById("radio2"),
-  document.getElementById("radio3"),
-];
-const buttonStart = document.getElementById("start-button");
+    document.getElementById("radio1"),
+    document.getElementById("radio2"),
+    document.getElementById("radio3"),
+]
+const buttonStart = document.getElementById("start-button")
 
-let main = document.getElementById("main");
-let selectedDifficultyLevel;
+let main = document.getElementById("main")
+let selectedDifficultyLevel
 
 difficultyLevels.forEach((level, index) => {
-  level.addEventListener("click", () => {
-    selectedDifficultyLevel = index + 1;
-  });
-});
+    level.addEventListener("click", () => {
+        selectedDifficultyLevel = index + 1
+    })
+})
 
 buttonStart.addEventListener("click", () => {
-  if (selectedDifficultyLevel === 1) {
-    main.innerHTML = `Вы выбрали легкий уровень`;
-  } else if (selectedDifficultyLevel === 2) {
-    main.innerHTML = `Вы выбрали средний уровень`;
-  } else if (selectedDifficultyLevel === 3) {
-    main.innerHTML = `Вы выбрали сложный уровень`;
-  }
-});
+    if (selectedDifficultyLevel === 1) {
+        main.innerHTML = `Вы выбрали легкий уровень`
+    } else if (selectedDifficultyLevel === 2) {
+        main.innerHTML = `Вы выбрали средний уровень`
+    } else if (selectedDifficultyLevel === 3) {
+        main.innerHTML = `Вы выбрали сложный уровень`
+    }
+})
